e4495b70b4090df8fd4e9a2f842fe8b49b8d5f15
Sakshi Choudhary <choudharysakshi023@gmail.com>
2022-06-16 23:23:17 +0530

    ⚡️ add cli log

--- kafka-cli/1-kafka-console-producer.sh
+++ kafka-cli/1-kafka-console-producer.sh
@@ -32,3 +32,49 @@ hello again!
 kafka-topics --zookeeper 127.0.0.1:2181 --list
 kafka-topics --zookeeper 127.0.0.1:2181 --topic new_topic_2 --describe
 
+
+
+# Last login: Thu Jun 16 17:36:22 on ttys006
+# ➜  ~ kafka-topics.sh --bootstrap-server localhost:9092 --list
+
+# ➜  ~ kafka-topics.sh --bootstrap-server localhost:9092 --create --topic myfirsttopic
+# Created topic myfirsttopic.
+# ➜  ~ kafka-topics.sh --bootstrap-server localhost:9092 --create --topic mysecondtopic --partitions 3
+# Created topic mysecondtopic.
+# ➜  ~ kafka-topics.sh --bootstrap-server localhost:9092 --create --topic mythirdtopic --partitions 3 --replication-factor 2
+# Error while executing topic command : Replication factor: 2 larger than available brokers: 1.
+# [2022-06-16 18:01:54,135] ERROR org.apache.kafka.common.errors.InvalidReplicationFactorException: Replication factor: 2 larger than available brokers: 1.
+#  (kafka.admin.TopicCommand$)
+# ➜  ~ kafka-topics.sh --bootstrap-server localhost:9092 --create --topic mythirdtopic --partitions 3 --replication-factor 1
+# Created topic mythirdtopic.
+# ➜  ~ kafka-topics.sh --bootstrap-server localhost:9092 --list
+# myfirsttopic
+# mysecondtopic
+# mythirdtopic
+# ➜  ~ kafka-topics.sh --bootstrap-server localhost:9092 --describe --topic mythirdtopic
+# Topic: mythirdtopic	TopicId: csoCe4FmT1-q-j0JNHlVgA	PartitionCount: 3	ReplicationFactor: 1	Configs: 
+# 	Topic: mythirdtopic	Partition: 0	Leader: 0	Replicas: 0	Isr: 0
+# 	Topic: mythirdtopic	Partition: 1	Leader: 0	Replicas: 0	Isr: 0
+# 	Topic: mythirdtopic	Partition: 2	Leader: 0	Replicas: 0	Isr: 0
+# ➜  ~ kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic myfirsttopic
+# ➜  ~ kafka-topics.sh --bootstrap-server localhost:9092 --list                         
+# mysecondtopic
+# mythirdtopic        
+# ➜  ~ kafka-topics.sh --bootstrap-server localhost:9092 --list
+# mysecondtopic
+# mythirdtopic
+# ➜  ~ kafka-console-producer.sh --bootstrap-server localhost:9092 --topic mysecondtopic
+# >Hello cutiess
+# >I'm sakshi
+# >I'm loving kafka
+# >^C%                                                                                                                                     ➜  ~ kafka-console-producer.sh --bootstrap-server localhost:9092 --topic mysecondtopic --producer-property acks=all
+# >some acked mssg
+# >for fun
+# >learning stuff!!!
+# >567fjvj
+# >^C%                                                                                                                                  ➜  ~ kafka-console-producer.sh --bootstrap-server localhost:9092 --topic mysecondtopic                            
+# >heyo
+# >do u copy that?
+# >GG
+# >efds
+# >
--- kafka-cli/2-kafka-console-consumer.sh
+++ kafka-cli/2-kafka-console-consumer.sh
@@ -8,3 +8,33 @@ kafka-console-producer --broker-list 127.0.0.1:9092 --topic first_topic
 
 # consuming from beginning
 kafka-console-consumer --bootstrap-server 127.0.0.1:9092 --topic first_topic --from-beginning
+
+# displaykey,values and timestamp
+kafka-console-consumer.sh --bootstrap-server 127.0.0.1:9092 --topic first_topic --formatter 
+kafka.tools.DefaultMessageFormatter 
+--property print.timestamp=true 
+--property print.timestamp=true 
+--property print.key=true 
+--property print.value=true 
+--from-beginning
+
+# Last login: Thu Jun 16 17:56:53 on ttys000
+# ➜  ~ kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic mysecondtopic
+# heyo
+# do u copy that?
+# GG
+# gfefds
+# ^CProcessed a total of 4 messages
+# ➜  ~ kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic mysecondtopic --from-beginning
+# Hello cutiess
+# 567fjvj
+# do u copy that?
+# I'm sakshi
+# for fun
+# heyo
+# efds
+# I'm loving kafka
+# some acked mssg
+# learning stuff!!!
+# GG
+
