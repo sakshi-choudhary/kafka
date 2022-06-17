447537df881ba6ad6e1dbe6dc38875bb76a6151d
Sakshi Choudhary <choudharysakshi023@gmail.com>
2022-06-18 00:23:46 +0530

    ⚡️ add cli logs

--- kafka-cli/4-kafka-consumer-groups.sh
+++ kafka-cli/4-kafka-consumer-groups.sh
@@ -24,3 +24,48 @@ kafka-console-consumer --bootstrap-server 127.0.0.1:9092 --topic first_topic --g
 
 # describe the group again
 kafka-consumer-groups --bootstrap-server localhost:9092 --describe --group my-first-application
+
+
+#~ kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my_first_consumer_grp
+
+# Consumer group 'my_first_consumer_grp' has no active members.
+
+# GROUP                 TOPIC           PARTITION  CURRENT-OFFSET  LOG-END-OFFSET  LAG             CONSUMER-ID     HOST            CLIENT-ID
+# my_first_consumer_grp mysecondtopic   0          6               6               0               -               -               -
+# my_first_consumer_grp mysecondtopic   1          5               5               0               -               -               -
+# my_first_consumer_grp mysecondtopic   2          9               9               0               -               -               -
+# ➜  ~ clear
+
+# ➜  ~ kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my_first_consumer_grp
+
+# Consumer group 'my_first_consumer_grp' has no active members.
+
+# GROUP                 TOPIC           PARTITION  CURRENT-OFFSET  LOG-END-OFFSET  LAG             CONSUMER-ID     HOST            CLIENT-ID
+# my_first_consumer_grp mysecondtopic   0          6               6               0               -               -               -
+# my_first_consumer_grp mysecondtopic   1          5               5               0               -               -               -
+# my_first_consumer_grp mysecondtopic   2          9               9               0               -               -               -
+# ➜  ~ kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my_first_consumer_grp
+
+# Consumer group 'my_first_consumer_grp' has no active members.
+
+# GROUP                 TOPIC           PARTITION  CURRENT-OFFSET  LOG-END-OFFSET  LAG             CONSUMER-ID     HOST            CLIENT-ID
+# my_first_consumer_grp mysecondtopic   0          6               8               2               -               -               -
+# my_first_consumer_grp mysecondtopic   1          5               6               1               -               -               -
+# my_first_consumer_grp mysecondtopic   2          9               11              2               -               -               -
+# ➜  ~ kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic mysecondtopic --group my_first_consumer_grp
+# frth msggg
+# one message
+# thrd msg
+# sec msg
+# fvth msgggg!
+# hiiii
+# ^CProcessed a total of 6 messages
+# ➜  ~ kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my_first_consumer_grp          
+
+# Consumer group 'my_first_consumer_grp' has no active members.
+
+# GROUP                 TOPIC           PARTITION  CURRENT-OFFSET  LOG-END-OFFSET  LAG             CONSUMER-ID     HOST            CLIENT-ID
+# my_first_consumer_grp mysecondtopic   0          8               8               0               -               -               -
+# my_first_consumer_grp mysecondtopic   1          6               6               0               -               -               -
+# my_first_consumer_grp mysecondtopic   2          12              12              0               -               -               -
+# ➜  ~ 
